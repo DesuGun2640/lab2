079e1d6e4ea116fd68c1dd05013399d1344caf6d
unknown <admin@VLAN-25-IKK-055.rd.mirea.ru>
2017-03-31 14:08:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,57 @@
+import javax.activation.MimeType;
+import javax.activation.MimeTypeParseException;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) throws URISyntaxException, IOException, MimeTypeParseException {
+        download(new URI("https://www.mirea.ru/"), new HashSet<>());
+    }
+
+    private static void download(URI link, HashSet<URI> visited){
+        if  (visited.contains(link) || visited.size() >=16) return;
+        visited.add(link);
+        System.out.println(link);
+        try {
+            URL url = link.toURL();
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            String ct = conn.getHeaderField("Content-Type");
+            MimeType mt = new MimeType(ct);
+            String cs = mt.getParameter("charset");
+            String html;
+            try (InputStream is = conn.getInputStream()) {
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                while (true) {
+                    int c = is.read();
+                    if (c < 0) break;
+                    bos.write(c);
+                }
+                html = cs == null ? bos.toString() : bos.toString(cs);
+            }
+            Pattern p = Pattern.compile("href\\s*=\\s*([^\\s>]+|'[^']*'|\"[^\"]*\")");
+            Matcher m = p.matcher(html);
+            while (m.find()) {
+                String href = m.group(1);
+                if (href.startsWith("'") || href.startsWith("\"")) {
+                    href = href.substring(1, href.length() - 1);
+                }
+                URI child = link.resolve(href);
+                //download(child, visited);
+                Runnable action = () -> download(child, visited);
+                new Thread(action).start();
+            }
+        } catch (Exception error) {
+            System.out.println(error);
+        }
+    }
+
+
+}
